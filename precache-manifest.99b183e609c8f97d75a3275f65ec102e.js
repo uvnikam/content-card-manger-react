ad77bc589e8fabd5c1c8864004aff328cec61ca3
uvnikam <42571089+uvnikam@users.noreply.github.com>
2018-10-19 02:27:57 +0530

    Add files via upload

---
+++ precache-manifest.99b183e609c8f97d75a3275f65ec102e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "705cbad9b0b66e7ebf76",
+    "url": "/static/css/main.af49bdc3.chunk.css"
+  },
+  {
+    "revision": "a09347df4e7d5988008a7a25b4ab7dcb",
+    "url": "/static/media/homepage-marquee.a09347df.jpg"
+  },
+  {
+    "revision": "162a9916c0e17edc3a58",
+    "url": "/static/js/1.162a9916.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a18f7a1f2a9a4ef8c3e7b6538ded6906",
+    "url": "/static/media/infobip_logo_png_big.a18f7a1f.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "705cbad9b0b66e7ebf76",
+    "url": "/static/js/main.705cbad9.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "0086576e17122471eb583bbc481b7ef6",
+    "url": "/static/media/card_bg.0086576e.jpg"
+  },
+  {
+    "revision": "162a9916c0e17edc3a58",
+    "url": "/static/css/1.67a9dc20.chunk.css"
+  },
+  {
+    "revision": "b3961f3142740933ec1281bcc8c7ec3b",
+    "url": "/index.html"
+  }
+];
